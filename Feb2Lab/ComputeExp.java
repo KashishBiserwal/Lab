a12468f6d67128ad20072628a83cd3e63201b69d
KashishBiserwal <112905564+KashishBiserwal@users.noreply.github.com>
2023-02-02 17:48:21 +0530

    Update ComputeExp.java

--- Feb2Lab/ComputeExp.java
+++ Feb2Lab/ComputeExp.java
@@ -3,6 +3,10 @@ package Feb2Lab;
 //(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)
 public class ComputeExp {
     public static void main(String[] args) {
-        System.out.print("The value of given expression: "+ (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        float a = 25.5f;
+        float b = 3.5f;
+        float c = 40.5f - 4.5f;
+        float ans = (a*b - b*b)/c;
+        System.out.println(ans);
     }
 }
